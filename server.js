c6e77fc88c3f64678c6e368d6c30bfa8347efd2c
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-25 16:42:12 +0530

    Started Work ^.^

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('<h1> This is the first Test Express App </h1>') 
 });
-const PORT =8080;
+const PORT =process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
