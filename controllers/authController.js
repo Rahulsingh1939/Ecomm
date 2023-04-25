fd61d4201e51986e82bbdb11b38080419d091ec2
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-26 02:43:05 +0530

    Work Started on MVP Pattern

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,10 @@
 import userModel from '../models/userModel.js';
 
-export const registerController = async () => {};
+export const registerController = async (req,res) => {
+    const {name , email, password, phone,address} = req.body;
+
+    //Checking if all the fields are filled
+    if(!name || !email || !password || !phone || !address){
+        return res.status(400).json({error: "Please fill all the fields"});
+    }
+};
