8e96fbbc84efd741bea9ba5a1ed93640d84c842d
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-26 02:06:43 +0530

    Work Started on MVP Pattern

--- models/userModel.js
+++ models/userModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name:{
         type: String,
-        required: true
+        required: true,
+        trim: true,
     }
 })
