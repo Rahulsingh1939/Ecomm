8e7469870742b67f71b7b4dbc59b37e689b90669
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-26 02:38:18 +0530

    Work Started on MVP Pattern

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcryptjs';
 export const hashPassword = (password) => {
     try{
         const saltrounds = 10;
-        
-    }
+        const hashedPassword = bcrypt.hashSync(password,saltrounds);
+        return hashedPassword;
+    },
+    catch()
 }
