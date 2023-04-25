84f0601967877f01ccd117c626ca20fac0773c05
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-25 19:01:42 +0530

    Started Work on DataBase ^.^

--- config/db.js
+++ config/db.js
@@ -4,5 +4,7 @@ const connectDB = async () => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error)
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
 }
