2f8a2fa9e792f01e8817d738b794d990926f3669
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-26 02:45:35 +0530

    Work Started on MVP Pattern

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,4 +7,13 @@ export const registerController = async (req,res) => {
     if(!name || !email || !password || !phone || !address){
         return res.status(400).json({error: "Please fill all the fields"});
     }
+
+    //Checking if the user already exists
+    const existingUser = await userModel.findOne({email});
+    if(existingUser){
+        return res.status(200).send({
+            sucess:true,
+            message: "User already exists"
+        });
+    }
 };
