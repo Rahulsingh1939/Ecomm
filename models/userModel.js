572d5016f3db8b64bc009d2107ea7294d31006bb
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-26 02:09:45 +0530

    Work Started on MVP Pattern

--- models/userModel.js
+++ models/userModel.js
@@ -10,5 +10,21 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         email: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: Number,
+        default: 0
     }
 })
