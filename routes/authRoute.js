70f1b9901fc9c821a5691def22d0abccec260289
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-27 10:54:15 +0530

    Added Registering function

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import { registerController,loginController } from "../controllers/authController.js";
 //router  object
 const router = express.Router();
+//Routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
 
-router.post('/register',registerController);
+//LOGIN || POST
+router.post("/login", loginController);
+
 export default router;
