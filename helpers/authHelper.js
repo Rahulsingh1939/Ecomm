efd676e8a2173946b24c94d4ca913567b617dc11
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-27 18:03:42 +0530

    Completing the project

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,19 +1,15 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
-export const hashPassword = (password) => {
-    try{
-        const saltrounds = 10;
-        const hashedPassword = bcrypt.hashSync(password,saltrounds);
-        return hashedPassword;
-    }    catch(error){
-        console.log(error);
-    }
-}
+export const hashPassword = async (password) => {
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const comparePassword = async (password, hashedPassword) => {
-    try{
-        return bcrypt.compare(password, hashedPassword);
-    }    catch(error){
-        console.log(error);
-    }
-}
+  return bcrypt.compare(password, hashedPassword);
+};
