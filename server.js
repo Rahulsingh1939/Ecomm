2d6646314157b832bb04fdaa67188c54b1807759
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-27 18:12:11 +0530

    Finalizing Project

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/db');
-// const morgan = require('morgan');
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import morgan from 'morgan';
-import authRoute from './routes/authRoute.js';
-
-const app = express();
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
 //configure env
 dotenv.config();
 
-//connect to database
+//databse config
 connectDB();
 
-//middleware
+//rest object
+const app = express();
+
+//middelwares
+app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //routes
-app.use('/api/v1/auth', authRoute);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
-//API 
-app.get('/', (req, res) => {
-    res.send(`<h1> This is the first Test Express App running on ${PORT} </h1>`) 
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
 });
-const PORT =process.env.PORT || 8080;
 
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
