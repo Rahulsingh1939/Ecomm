783db22ad4dc850a7f354dba00978f5c07fb38b2
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-27 11:18:52 +0530

    Added Login function

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,4 +10,11 @@ router.post("/register", registerController);
 //LOGIN || POST
 router.post("/login", loginController);
 
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+//protected route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+    res.status(200).send({ ok: true });
+  });
+
 export default router;
