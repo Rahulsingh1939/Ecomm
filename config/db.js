f0fd4af3c8da090420dfa65cbe7d45862984d936
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-27 17:59:26 +0530

    Started Completing the project

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
-// const mongoose = require('mongoose');
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
+import colors from "colors";
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  }
+};
+
 export default connectDB;
