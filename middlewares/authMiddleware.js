9d21312624877f013468d9ff80094b06b6fcc986
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-04-27 18:04:05 +0530

    Completing the project

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,35 +3,10 @@ import userModel from "../models/userModel.js";
 
 //Protected Routes token base
 export const requireSignIn = async (req, res, next) => {
-
-    const token = req.headers.authorization.split(" ")[1];
-    console.log(token);
-    // console.log(token);
-    // if (!token) {
-    //     return res.status(401).send({
-    //         success: false,
-    //         message: "Token is Required",
-    //     }); 
-    // }
-    // try {
-    //     const decode = await JWT.verify(token, process.env.JWT_SECRET);
-    //     req.user = decode;
-    //     next();
-    // } catch (error) {
-    //     console.log(error);
-    //     res.status(401).send({
-    //         success: false,
-    //         message: "Invalid Token",
-    //         error,
-    //     });
-    // }
-
   try {
-    // console.log(req.headers.authorization);
-    // console.log(process.env.JWT_SECRET);
     const decode = JWT.verify(
-      token,
-        process.env.JWT_SECRET
+      req.headers.authorization,
+      process.env.JWT_SECRET
     );
     req.user = decode;
     next();
